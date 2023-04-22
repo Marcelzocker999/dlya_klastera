d60845a4946e8227f43949a4b90d0d1ecdcf2f10
Marcelzocker999 <103364949+Marcelzocker999@users.noreply.github.com>
2023-04-22 22:05:30 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/grid/testbed/tb410/radixsort-main
+ml icc
+for i in {1..3}; do
+  scr=$(date +"%A_%H:%M:%S_%N")
+  icc -O$i c.cpp -o try_$scr
+  echo "Iteration $i:"
+  time ./try_$scr
+done
+`
