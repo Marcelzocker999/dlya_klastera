b252f0d826d134f946113010ca6ee3391184f07c
Marcelzocker999 <103364949+Marcelzocker999@users.noreply.github.com>
2023-04-22 18:31:17 +0300

    Update F1.c

--- F1.c
+++ F1.c
@@ -17,29 +17,15 @@ void add_matrices(int a[][COLS], int b[][COLS], int c[][COLS]) {
 int main() {
     int a[ROWS][COLS], b[ROWS][COLS], c[ROWS][COLS];
     int i, j, k;
-
-    // initialize matrices a and b with random values
-    srand(time(NULL));
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
-            a[i][j] = rand() % 10;
-            b[i][j] = rand() % 10;
+    for (k = 0; k < 1000000000000; k++) {
+        srand(time(NULL));
+        for (i = 0; i < ROWS; i++) {
+            for (j = 0; j < COLS; j++) {
+                a[i][j] = rand() % 10;
+                b[i][j] = rand() % 10;
+            }
         }
-    }
-
-    // add matrices 10,000 times
-    for (k = 0; k < 10000; k++) {
         add_matrices(a, b, c);
     }
-
-    // print the final matrix c
-    printf("Matrix C:\n");
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
-            printf("%d ", c[i][j]);
-        }
-        printf("\n");
-    }
-
     return 0;
 }
