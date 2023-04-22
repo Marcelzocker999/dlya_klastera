cb850e1d732401c9be0a00ab11f43ce8e4cd5982
Marcelzocker999 <103364949+Marcelzocker999@users.noreply.github.com>
2023-04-22 22:08:38 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /home/grid/testbed/tb410/radixsort-main
+cd /home/grid/testbed/tb117/vec_samples/src
 ml icc
 for i in {1..3}; do
   scr=$(date +"%A_%H:%M:%S_%N")
-  icc -O$i c.cpp -o try_$scr
+  icc -O$i F1.c -o try_$scr
   echo "Iteration $i:"
   time ./try_$scr
 done
