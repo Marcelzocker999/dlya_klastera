082650af08274428968ba35cd331bcba4000fa8f
Marcelzocker999 <103364949+Marcelzocker999@users.noreply.github.com>
2023-04-22 22:11:45 +0300

    1
    2

--- bash.sh
+++ bash.sh
@@ -2,10 +2,9 @@
 cd /home/grid/testbed/tb117/vec_samples/src
 ml icc
 for i in {1..3}; do
-  scr=$(date +"%A_%H:%M:%S_%N")
-  icc -O$i F1.c -o try_$scr
-  echo "Iteration $i:"
-  time ./try_$scr
+scr=$(date +"%A_%H:%M:%S_%N")
+icc -O$i F1.c -o try_$scr
+echo "Iteration $i:"
+time ./try_$scr
 done
 `
-
